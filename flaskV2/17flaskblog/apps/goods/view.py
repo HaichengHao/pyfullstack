b71cfc29719440161e984f5a368c9d6bb4d2e629
aya <2020104216@qq.com>
2025-08-29 17:53:33 +0800

    20250829B

--- flaskV2/17flaskblog/apps/goods/view.py
+++ flaskV2/17flaskblog/apps/goods/view.py
@@ -2,9 +2,9 @@
 # @FileName  :view.py
 # @DateTime  :2025/8/28 21:22
 
-from flask import Blueprint, request,render_template
+from flask import Blueprint, request, render_template
 
-from apps.goods.models import Goods
+from apps.goods.models import Goods, User_goods
 from apps.user.models import User
 
 goods_bp = Blueprint(name='goods', import_name=__name__)
@@ -28,4 +28,13 @@ def finduser_route():
 def show_route():
     users = User.query.filter(User.isdelete == False).all()
     goods = Goods.query.all()
-    return render_template('goods/show.html',users=users,goods=goods)
+    return render_template('goods/show.html', users=users, goods=goods)
+
+
+@goods_bp.route('/buy', endpoint='buy')
+def but_route():
+    uid = request.args.get('user_id')
+    gid = request.args.get('goods_id')
+    Ug = User_goods()  # 创建表对象
+    Ug.user_id = uid
+    Ug.goods_id = gid
